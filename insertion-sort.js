eb0cf4aba12632d28aaa02e630d67bd4186b13b0
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-03 23:32:56 -1000

    working test used from last sort and working insertion

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,10 +1,19 @@
 /*jshint esversion: 6*/
 
-let myArray = [9,2,5,6,4,3,7,10,1,8];
+// let myArray = [9,2,5,6,4,3,7,10,1,8];
 
 function insertionSort(myArray) {
-  // body...
+  for (var i = 0; i < myArray.length; i++) {
+    var temp = myArray[i];
+    for (var j = i - 1; j >= 0 && (myArray[j] > temp); j--) {
+      myArray[j+1] = myArray[j];
+    }
+    myArray[j+ 1] = temp;
+    console.log(myArray);
+  }
+  return myArray;
 }
 
+// console.log(insertionSort(myArray));
 
 module.exports = insertionSort;
--- test/test-script.js
+++ test/test-script.js
@@ -7,8 +7,37 @@ chai.should();
 const insertionSort = require('../insertion-sort.js');
 
 describe('The Cases of Edge', ()=>{
+
   it('should be a function', ()=>{
     insertionSort.should.be.a('function');
+  });
+
+  it('shoud be not be an array', () => {
+    insertionSort.should.not.be.an('array');
+  });
+
+  it('should not be a boolean',()=>{
+    insertionSort.should.not.be.a('boolean');
+  });
+
+  it('should not be a string', ()=>{
+    insertionSort.should.not.be.a('string');
+  });
+
+  it('should not be null', ()=>{
+    insertionSort.should.not.equal('null');
+  });
+
+  it('should return an array', ()=>{
+    insertionSort([9,2,5,6,4,3,7,10,1,8]).should.be.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should not return an object', ()=>{
+    insertionSort([{}, {}, {},{}]).should.not.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should not return the same numbers', ()=>{
+    insertionSort([1,1,1,1,1,1,1]).should.not.deep.equal([1,2,3,4,5,6,7,8,9,10]);
   });
 });
 
