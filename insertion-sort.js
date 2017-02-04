c5c730fcb1887c27f3f49320bd2349824d36f13d
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-03 22:47:48 -1000

    added started scripts for test and insertion js

---
+++ insertion-sort.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6*/
+
+let myArray = [9,2,5,6,4,3,7,10,1,8];
+
+function insertionSort(myArray) {
+  // body...
+}
+
+
+// module.exports = insertionSort;
---
+++ test/test-script.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6*/
+
+const chai = require('chai');
+
+chai.should();
+
+const insertionSort = require('../insertion-sort.js');
+
+describe('The Cases of Edge', ()=>{
+  it('should be a function', ()=>{
+
+  });
+});
+
